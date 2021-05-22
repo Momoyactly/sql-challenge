fcb78bf19bc73dc13d6c124ec1f2ec849d95dcd2
Uriel Arriaga <uriel.arriaga@momodevops.dev>
2021-05-22 02:08:16 -0500

    fixed relationship Employees - Titles

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -1,33 +1,33 @@
-﻿--DROP TABLE IF EXISTS dept_emp_junction ;
---DROP TABLE IF EXISTS dept_manager_junction ;
---DROP TABLE IF EXISTS Employees;
---DROP TABLE IF EXISTS Departments;
---DROP TABLE IF EXISTS Salaries;
---DROP TABLE IF EXISTS Titles;
-
-CREATE TABLE  Employees  (
-     id  int PRIMARY KEY  NOT NULL,
-     Emp_no  int UNIQUE  NOT NULL,
-     Emp_title_id  VARCHAR(255)   NOT NULL,
-     Birth_date  VARCHAR(255)   NOT NULL,
-     First_name  VARCHAR(255)   NOT NULL,
-     Last_name  VARCHAR(255)   NOT NULL,
-     Sex  VARCHAR(255)   NOT NULL,
-     Hire_date  VARCHAR(255)   NOT NULL
-);
-
-CREATE TABLE  Salaries  (
-     id  int  PRIMARY KEY NOT NULL,
-     Emp_no  int   NOT NULL,
-     Salary  int   NOT NULL,
-	 FOREIGN KEY( Emp_no ) REFERENCES  Employees  ( Emp_no )
-);
+﻿DROP TABLE IF EXISTS dept_emp_junction ;
+DROP TABLE IF EXISTS dept_manager_junction ;
+DROP TABLE IF EXISTS Departments;
+DROP TABLE IF EXISTS Salaries;
+DROP TABLE IF EXISTS Employees;
+DROP TABLE IF EXISTS Titles;
 
 CREATE TABLE  Titles  (
      Title_id VARCHAR(255) PRIMARY KEY   NOT NULL,
      Title  VARCHAR(255)   NOT NULL    
 );
 
+CREATE TABLE  Employees  (
+     id  int PRIMARY KEY  NOT NULL,
+     Emp_no  int UNIQUE  NOT NULL,
+     Title_id  VARCHAR(255)   NOT NULL,
+     Birth_date  VARCHAR(255)   NOT NULL,
+     First_name  VARCHAR(255)   NOT NULL,
+     Last_name  VARCHAR(255)   NOT NULL,
+     Sex  VARCHAR(255)   NOT NULL,
+     Hire_date  VARCHAR(255)   NOT NULL,
+     FOREIGN KEY( Title_id ) REFERENCES  Titles  ( Title_id )
+);
+
+CREATE TABLE  Salaries  (
+     id  int  PRIMARY KEY NOT NULL,
+     Emp_no  int   NOT NULL,
+     Salary  int   NOT NULL,
+	FOREIGN KEY( Emp_no ) REFERENCES  Employees  ( Emp_no )
+);
 
 CREATE TABLE  Departments  (
      Dept_no  VARCHAR(255) PRIMARY KEY  NOT NULL,
@@ -38,8 +38,8 @@ CREATE TABLE  Dept_emp_juncion  (
      id  int PRIMARY KEY  NOT NULL,
      emp_no  int   NOT NULL,
      Dept_no  VARCHAR(255)   NOT NULL,
-	 FOREIGN KEY( emp_no )  REFERENCES  Employees  ( Emp_no ),
-	 FOREIGN KEY( Dept_no ) REFERENCES  Departments  ( Dept_no ) 
+	FOREIGN KEY( emp_no )  REFERENCES  Employees  ( Emp_no ),
+	FOREIGN KEY( Dept_no ) REFERENCES  Departments  ( Dept_no ) 
 );
 
 CREATE TABLE  Dept_manager_junction  (
@@ -47,7 +47,7 @@ CREATE TABLE  Dept_manager_junction  (
      Dept_no  VARCHAR(255)   NOT NULL,
      Emp_no  int   NOT NULL,
      FOREIGN KEY( Dept_no ) REFERENCES  Departments  ( Dept_no ),
-	 FOREIGN KEY( Emp_no ) REFERENCES  Employees  ( Emp_no )
+	FOREIGN KEY( Emp_no ) REFERENCES  Employees  ( Emp_no )
 );
 
 
